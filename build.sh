22ae2940e22a612d2b47377007c2cdc02731bb8a
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-25 14:26:42 +0100

    Force destroy before build

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ name=`cat box-name.txt`
 version=`git describe`
 
 echo "Building box ${name} version: ${version} ..."
-vagrant destroy
+vagrant destroy -f
 vagrant up
 vagrant package --vagrantfile VagrantfileToInclude --output "${name}-${version}.box"
